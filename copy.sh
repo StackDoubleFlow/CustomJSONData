20a50833f1e4a7bddbb4ea040c7e92443eee8e5e
StackDoubleFlow <ojaslandge@gmail.com>
2020-12-03 17:20:06 -0600

    create copy and buildBMBF scripts

---
+++ copy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NDKPath=`cat ndkpath.txt`
+
+buildScript="$NDKPath/build/ndk-build"
+
+./$buildScript NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk NDK_APPLICATION_MK=./Application.mk -j8
+adb push libs/arm64-v8a/libcustom-json-data.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libcustom-json-data.so
+adb shell am force-stop com.beatgames.beatsaber
+adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
+
+adb logcat -c && adb logcat > test.log
