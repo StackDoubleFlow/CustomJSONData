d31972180d3f4f5617de8c5f10003dba4f81ecd0
StackDoubleFlow <ojaslandge@gmail.com>
2021-07-23 17:07:43 -0500

    Update to beatsaber 1.16.3

--- Android.mk
+++ Android.mk
@@ -18,24 +18,24 @@ TARGET_ARCH_ABI := $(APP_ABI)
 
 rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
 
-# Creating prebuilt for dependency: modloader - version: 1.1.0
+# Creating prebuilt for dependency: modloader - version: 1.2.3
 include $(CLEAR_VARS)
 LOCAL_MODULE := modloader
 LOCAL_EXPORT_C_INCLUDES := extern/modloader
 LOCAL_SRC_FILES := extern/libmodloader.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: beatsaber-hook - version: 2.0.3
+# Creating prebuilt for dependency: beatsaber-hook - version: 2.1.3
 include $(CLEAR_VARS)
-LOCAL_MODULE := beatsaber-hook_2_0_3
+LOCAL_MODULE := beatsaber-hook_2_1_3
 LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
-LOCAL_SRC_FILES := extern/libbeatsaber-hook_2_0_3.so
+LOCAL_SRC_FILES := extern/libbeatsaber-hook_2_1_3.so
 LOCAL_CPP_FEATURES += exceptions
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: codegen - version: 0.10.2
+# Creating prebuilt for dependency: codegen - version: 0.11.0
 include $(CLEAR_VARS)
-LOCAL_MODULE := codegen_0_10_2
+LOCAL_MODULE := codegen_0_11_0
 LOCAL_EXPORT_C_INCLUDES := extern/codegen
-LOCAL_SRC_FILES := extern/libcodegen_0_10_2.so
+LOCAL_SRC_FILES := extern/libcodegen_0_11_0.so
 LOCAL_CPP_FEATURES += exceptions
 include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: custom-types - version: 0.12.4
@@ -54,8 +54,8 @@ LOCAL_SRC_FILES += $(call rwildcard,src/,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
 LOCAL_SHARED_LIBRARIES += modloader
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_2_0_3
-LOCAL_SHARED_LIBRARIES += codegen_0_10_2
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_2_1_3
+LOCAL_SHARED_LIBRARIES += codegen_0_11_0
 LOCAL_SHARED_LIBRARIES += custom-types
 LOCAL_LDLIBS += -llog
 LOCAL_CFLAGS += -I"include" -I"shared" -I"./extern/libil2cpp/il2cpp/libil2cpp" -isystem"extern" -isystem"./extern/codegen/include" -DVERSION='"0.3.1"'
