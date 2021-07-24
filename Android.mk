189d5b16f80424c6de3fbf3cbfbe15d3e2b7dbe5
StackDoubleFlow <ojaslandge@gmail.com>
2021-07-24 04:34:00 -0500

    Update codegen

--- Android.mk
+++ Android.mk
@@ -24,18 +24,18 @@ LOCAL_MODULE := modloader
 LOCAL_EXPORT_C_INCLUDES := extern/modloader
 LOCAL_SRC_FILES := extern/libmodloader.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: beatsaber-hook - version: 2.1.3
+# Creating prebuilt for dependency: beatsaber-hook - version: 2.1.4
 include $(CLEAR_VARS)
-LOCAL_MODULE := beatsaber-hook_2_1_3
+LOCAL_MODULE := beatsaber-hook_2_1_4
 LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
-LOCAL_SRC_FILES := extern/libbeatsaber-hook_2_1_3.so
+LOCAL_SRC_FILES := extern/libbeatsaber-hook_2_1_4.so
 LOCAL_CPP_FEATURES += exceptions
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: codegen - version: 0.11.0
+# Creating prebuilt for dependency: codegen - version: 0.11.1
 include $(CLEAR_VARS)
-LOCAL_MODULE := codegen_0_11_0
+LOCAL_MODULE := codegen_0_11_1
 LOCAL_EXPORT_C_INCLUDES := extern/codegen
-LOCAL_SRC_FILES := extern/libcodegen_0_11_0.so
+LOCAL_SRC_FILES := extern/libcodegen_0_11_1.so
 LOCAL_CPP_FEATURES += exceptions
 include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: custom-types - version: 0.12.4
@@ -54,8 +54,8 @@ LOCAL_SRC_FILES += $(call rwildcard,src/,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
 LOCAL_SHARED_LIBRARIES += modloader
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_2_1_3
-LOCAL_SHARED_LIBRARIES += codegen_0_11_0
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_2_1_4
+LOCAL_SHARED_LIBRARIES += codegen_0_11_1
 LOCAL_SHARED_LIBRARIES += custom-types
 LOCAL_LDLIBS += -llog
 LOCAL_CFLAGS += -I"include" -I"shared" -I"./extern/libil2cpp/il2cpp/libil2cpp" -isystem"extern" -isystem"./extern/codegen/include" -DVERSION='"0.3.1"'
