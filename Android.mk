2d2f8bd4e62da90ff383e6085f98955b76cc54b9
StackDoubleFlow <ojaslandge@gmail.com>
2021-07-27 22:24:01 -0500

    Update dependencies for 1.16.4

--- Android.mk
+++ Android.mk
@@ -24,21 +24,21 @@ LOCAL_MODULE := modloader
 LOCAL_EXPORT_C_INCLUDES := extern/modloader
 LOCAL_SRC_FILES := extern/libmodloader.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: beatsaber-hook - version: 2.1.4
+# Creating prebuilt for dependency: beatsaber-hook - version: 2.2.2
 include $(CLEAR_VARS)
-LOCAL_MODULE := beatsaber-hook_2_1_4
+LOCAL_MODULE := beatsaber-hook_2_2_2
 LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
-LOCAL_SRC_FILES := extern/libbeatsaber-hook_2_1_4.so
+LOCAL_SRC_FILES := extern/libbeatsaber-hook_2_2_2.so
 LOCAL_CPP_FEATURES += exceptions
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: codegen - version: 0.11.1
+# Creating prebuilt for dependency: codegen - version: 0.12.3
 include $(CLEAR_VARS)
-LOCAL_MODULE := codegen_0_11_1
+LOCAL_MODULE := codegen_0_12_3
 LOCAL_EXPORT_C_INCLUDES := extern/codegen
-LOCAL_SRC_FILES := extern/libcodegen_0_11_1.so
+LOCAL_SRC_FILES := extern/libcodegen_0_12_3.so
 LOCAL_CPP_FEATURES += exceptions
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: custom-types - version: 0.12.4
+# Creating prebuilt for dependency: custom-types - version: 0.12.5
 include $(CLEAR_VARS)
 LOCAL_MODULE := custom-types
 LOCAL_EXPORT_C_INCLUDES := extern/custom-types
@@ -54,8 +54,8 @@ LOCAL_SRC_FILES += $(call rwildcard,src/,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
 LOCAL_SHARED_LIBRARIES += modloader
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_2_1_4
-LOCAL_SHARED_LIBRARIES += codegen_0_11_1
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_2_2_2
+LOCAL_SHARED_LIBRARIES += codegen_0_12_3
 LOCAL_SHARED_LIBRARIES += custom-types
 LOCAL_LDLIBS += -llog
 LOCAL_CFLAGS += -I"include" -I"shared" -I"./extern/libil2cpp/il2cpp/libil2cpp" -isystem"extern" -isystem"./extern/codegen/include" -DVERSION='"0.3.1"'
