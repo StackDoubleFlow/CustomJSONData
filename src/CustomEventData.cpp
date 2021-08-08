b2cbe6a2c5298914d4cbd322616079fbcde0fc7a
StackDoubleFlow <ojaslandge@gmail.com>
2021-08-08 00:30:48 -0500

    Make new callbacks compile

--- src/CustomEventData.cpp
+++ src/CustomEventData.cpp
@@ -4,9 +4,11 @@ using namespace CustomJSONData;
 
 std::vector<CustomEventCallbackData> CustomEventCallbacks::customEventCallbacks;
 
-void CustomEventCallbacks::AddCustomEventCallback(void (*callback)(CustomJSONData::CustomEventData *), float aheadTime, bool callIfBeforeStartTime) {
+void CustomEventCallbacks::AddCustomEventCallback(
+    void (*callback)(GlobalNamespace::BeatmapObjectCallbackController *callbackController,
+                     CustomJSONData::CustomEventData *),
+    float aheadTime, bool callIfBeforeStartTime) {
+    
     CJDLogger::GetLogger().debug("Added custom event callback at %p", callback);
-    customEventCallbacks.push_back({
-        callback, aheadTime, callIfBeforeStartTime, 0
-    });
+    customEventCallbacks.push_back({callback, aheadTime, callIfBeforeStartTime, 0});
 }
