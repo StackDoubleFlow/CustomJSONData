948e8c428a2482aec4b82cab4bd35ad859a1856a
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2021-12-16 20:16:21 -0400

    Start migration to qpm-rust

--- copy.ps1
+++ copy.ps1
@@ -1,10 +1,8 @@
-$buildScript = "$env:ANDROID_NDK_LATEST_HOME/build/ndk-build"
-if (-not ($PSVersionTable.PSEdition -eq "Core")) {
-    $buildScript += ".cmd"
-}
+& $PSScriptRoot/build.ps1
 
-& $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
-& adb push libs/arm64-v8a/libcustom-json-data.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libcustom-json-data.so
-& adb shell am force-stop com.beatgames.beatsaber
-& adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
-adb logcat -c; adb logcat > test.log
+if ($?) {
+    & adb push build/libcustom-json-data.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libcustom-json-data.so
+    & adb shell am force-stop com.beatgames.beatsaber
+    & adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
+    adb logcat -c; adb logcat > test.log
+}
