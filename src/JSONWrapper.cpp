212302020f750b84fd568c03d024772489ad24fe
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2021-12-18 17:13:38 -0400

    Fix copying fields

--- src/JSONWrapper.cpp
+++ src/JSONWrapper.cpp
@@ -7,3 +7,21 @@ using namespace CustomJSONData;
 DEFINE_TYPE(CustomJSONData, JSONWrapper);
 DEFINE_TYPE(CustomJSONData, JSONWrapperUTF16);
 DEFINE_TYPE(CustomJSONData, DocumentWrapper);
+
+JSONWrapper *JSONWrapper::GetCopy() {
+    auto copy = JSONWrapper::New_ctor();
+
+    copy->value = value;
+    copy->associatedData = associatedData;
+
+    return copy;
+}
+
+JSONWrapperUTF16 *JSONWrapperUTF16::GetCopy() {
+    auto copy = JSONWrapperUTF16::New_ctor();
+
+    copy->value = value;
+    copy->associatedData = associatedData;
+
+    return copy;
+}
