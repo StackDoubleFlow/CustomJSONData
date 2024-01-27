a415cb44f23616c23d746c50b2f795605f86dc7b
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2024-01-27 00:48:45 -0400

    Fix copy destination

--- copy.ps1
+++ copy.ps1
@@ -1,7 +1,7 @@
 & $PSScriptRoot/build.ps1
 
 if ($?) {
-    & adb push build/libcustom-json-data.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libcustom-json-data.so
+    & adb push build/libcustom-json-data.so /sdcard/ModData/com.beatgames.beatsaber/Modloader/mods/libcustom-json-data.so
     & adb shell am force-stop com.beatgames.beatsaber
     & adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
     adb logcat -c; adb logcat > test.log
