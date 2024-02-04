72d9b6afceb9a41d3ec9e92fbbfe67d6a8ede5ff
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2024-02-04 10:02:41 -0400

    Use late_load

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ extern "C" void setup(CModInfo& info) {
   info = CustomJSONData::modInfo.to_c();
 }
 
-extern "C" void load() {
+extern "C" void late_load() {
   Paper::Logger::RegisterFileContextId(CJDLogger::Logger.tag, "CustomJSONDataPaper");
 
   CJDLogger::Logger.fmtLog<LogLevel::INF>("Installing CustomJSONData Hooks!");
