a237956cff104189ecb6053e151afb7330463f63
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2024-06-10 16:35:15 -0400

    Remove redundant fields

--- shared/CustomBeatmapData.h
+++ shared/CustomBeatmapData.h
@@ -147,8 +147,6 @@ DECLARE_INSTANCE_METHOD(CustomBeatmapData*, BaseCopy);
     DECLARE_INSTANCE_FIELD(bool, v2orEarlier);
     // optional
     DECLARE_INSTANCE_FIELD(CustomJSONData::JSONWrapper*, customData);
-    DECLARE_INSTANCE_FIELD(CustomJSONData::JSONWrapperUTF16*, beatmapCustomData);
-    DECLARE_INSTANCE_FIELD(CustomJSONData::JSONWrapperUTF16*, levelCustomData);
     DECLARE_INSTANCE_FIELD(CustomJSONData::DocumentWrapper*, doc);
   )
 
