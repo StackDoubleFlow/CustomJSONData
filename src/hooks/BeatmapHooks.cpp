a5c756c45ffd3840f25748ac30a567c262126458
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2024-06-10 11:44:56 -0400

    Fix include

--- src/hooks/BeatmapHooks.cpp
+++ src/hooks/BeatmapHooks.cpp
@@ -3,7 +3,6 @@
 
 #include "HookUtils.hpp"
 #include "CustomBeatmapData.h"
-#include "BeatmapDataLoaderUtils.hpp"
 
 #include "BeatmapSaveDataVersion2_6_0AndEarlier/zzzz__EventData_def.hpp"
 #include "BeatmapSaveDataVersion2_6_0AndEarlier/BeatmapSaveDataItem.hpp"
